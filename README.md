f65e8ade06ab25447e1aaaea3052e469e9e36efb
Andrea Carraro <andrea.carraro@xing.com>
2019-07-26 11:33:09 +0200

    Setup custom server to forward any request to single entrypoint

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Next.js + react-router
+
+- Install Next.js as usual
+- Configure custom `server.js` to redirect any request to same single entrypoint
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nextJS = require('next');
+
+async function start() {
+  const dev = process.env.NODE_ENV !== 'production';
+  const app = nextJS({ dev });
+  const server = express();
+  await app.prepare();
+
+  // Redirect all requests to main entrypoint pages/index.js
+  server.get('/*', async (req, res, next) => {
+    try {
+      app.render(req, res, '/');
+    } catch(e) {
+      next(e)
+    }
+  });
+
+  server.listen(3000, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:3000`);
+  });
+}
+
+start();
